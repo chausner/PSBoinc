1cacbe6b05e7e14a4c2dadac2ff20204caa75b9f
chausner <chausner@users.noreply.github.com>
2021-08-23 23:48:16 +0200

    Implement Update-BoincAccountManager

--- PSBoinc/AccountManagerCommands/UpdateBoincAccountManagerCommand.cs
+++ PSBoinc/AccountManagerCommands/UpdateBoincAccountManagerCommand.cs
@@ -1,16 +1,19 @@
-﻿namespace PSBoinc
+﻿using BoincRpc;
+using System;
+using System.Management.Automation;
+using System.Threading;
+
+namespace PSBoinc
 {
-    //[Cmdlet(VerbsData.Update, "BoincAccountManager")]
-    //public class UpdateBoincAccountManagerCommand : RpcCmdlet
-    //{
-    //    protected override void RpcProcessRecord()
-    //    {
-    //        // need to first add support for <use_config_file/> in RpcClient
+    [Cmdlet(VerbsData.Update, "BoincAccountManager")]
+    public class UpdateBoincAccountManagerCommand : RpcCmdlet
+    {
+       protected override void RpcProcessRecord()
+       {
+           AccountManagerRpcReply reply = RpcClient.AccountManagerUpdateAsync(CancellationToken.None).GetAwaiter().GetResult();
 
-    //        AccountManagerRpcReply reply = RpcClient.AccountManagerAttach(string.Empty, string.Empty, string.Empty, CancellationToken.None);
-
-    //        if (reply.ErrorCode != ErrorCode.Success)
-    //            throw new Exception(string.Format("Account manager operation failed. Error code: {0}. Error message: {1}", reply.ErrorCode, string.Join(" ", reply.Messages)));
-    //    }
-    //}
+           if (reply.ErrorCode != ErrorCode.Success)
+               throw new Exception(string.Format("Account manager operation failed. Error code: {0}. Error message: {1}", reply.ErrorCode, string.Join(" ", reply.Messages)));
+       }
+    }
 }
